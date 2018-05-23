cab3ea73f92b1d6f59b76588e419a71fd1487315
cswor <cswilburn@gmail.com>
2018-05-23 11:16:44 -0700

    Add comments to my submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,5 @@
-##
-##
-
-## Write a short comment describing this function
+## Creastes vector of functions needed to implement the
+## caching of the inv matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
     #myMat <- matrix(c(1,0,5,2,1,6,3,4,0), 3, 3)
@@ -20,7 +18,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Checks cache for the inv and
+## returns the cached inv if found
+## otherwise, solves for the inverse and caches it,
+## and returns the inv
 
 cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
@@ -33,11 +34,5 @@ cacheSolve <- function(x) {
     m <- solve(data)
     x$setinv(m)
     m
-
-######
-#    data <- x$get()
-#    m <- mean(data, ...)
-#    x$setmean(m)
-#    m
 
 }
